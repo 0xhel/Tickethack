0c73dd43da07ccf2472814a1e0571357e0ab3b99
Sterben <0xsterben@tutamail.com>
2024-04-16 17:58:56 +0200

    Route cart operationnelle

--- BACKEND/routes/cart.js
+++ BACKEND/routes/cart.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+const Cart = require('../models/carts');
+
+router.post('/cart', (req, res) => {
+
+    const newCart = new Cart({
+        id: req.body.id
+    })
+    newCart.save().then(saved => {
+        res.json({ result: true, trip: saved })
+    })
+})
+
 
 module.exports = router;
