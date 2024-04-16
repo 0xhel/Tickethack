acbe60f38b6f6190eae0b0c7ed89dab5becfda40
David.Kfld <david.s.kufeld@gmail.com>
2024-04-16 14:32:52 +0200

    modif module front

--- FRONTEND/modules.js
+++ FRONTEND/modules.js
@@ -1,28 +1,35 @@
-
-
-function searchTrip() {
-    const departInput = document.querySelector('#depart-input').value
-    const arrivalInput = document.querySelector('#arriv-input').value
-    const dateInput = document.querySelector('#date-input').value
+const data = {
+        departure :document.querySelector('#depart-input').value,
+        arrival : document.querySelector('#arriv-input').value,
+        date : document.querySelector('#date-input').value
+    }
+   
     
 
 document.querySelector('#search-btn').addEventListener('click', () => {
     fetch('http://localhost:3000/index', {
         method: 'POST',
        headers: { 'Content-Type': 'application/json' },
-       body: JSON.stringify({ departure, arrival, date})
+       body: JSON.stringify({data})
        
   })
   .then(data => data.json())
   .then( trip => {
-    console.log(trip)
-   const foundTrip = trip.filter(elem => elem.includes(elem.departure === departInput && elem.arrival === arrivalInput))
-   
+    for(let elem of trip) {
+        document.querySelector('#display-section').innerHTML += `
+        <div class="voyage">
+                        <div>${trip.arrival} > ${trip.arrival}</div>
+                        <div>${trip.date}</div>
+                        <div>${trip.price}</div>
+                        <button id="#book-btn">Book</button>
+                    </div>`
+    }   
   }
-   
-    
 )
-})}
+})
+
+
+
   
   
     
