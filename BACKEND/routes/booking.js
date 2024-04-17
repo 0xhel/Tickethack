5938c3b12bc75b4d8737c7b9ecb0e2b6f23eab1a
Sterben <0xsterben@tutamail.com>
2024-04-17 13:24:39 +0200

    J'ai resque finalisé la route booking mais je pense qu'il manque quelque chose

--- BACKEND/routes/booking.js
+++ BACKEND/routes/booking.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-const Trip = require('../models/trips');
+const Booking = require('../models/trips');
 
+router.post('/', (req, res) => {
+
+    const newBooking = new Booking({
+        booking: req.body.id
+    })
+    newBooking.save().then(saved => {
+        res.json({ result: true, booking: saved })
+    })
+})
+
+router.get('/', (req, res) => {
+    Booking.find()
+        .populate('trip')
+        .then(carts => {
+            res.json({ result: true, carts })
+        })
+
+})
 
 module.exports = router;
